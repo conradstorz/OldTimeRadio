68cb4d43890db809fb7b361e7e213f0aa3347a8d
Conrad Storz <conradstorz@gmail.com>
2016-02-23 09:07:20 -0500

    syntax error

--- play_radio.py
+++ play_radio.py
@@ -8,7 +8,7 @@ def play(directory):
     file = random.choice(os.listdir(directory))
     pygame.init()
     pygame.mixer.init()
-    pygame.mixer.music.load(file)
+    pygame.mixer.music.load(directory + file)
     pygame.mixer.music.play()
 
 play('./recordings/OTRadio/')
