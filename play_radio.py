f61ef75802c1c15781554899f3af12f5775289fb
Conrad Storz <conradstorz@gmail.com>
2016-02-24 22:02:11 -0500

    expanded list of functions

--- play_radio.py
+++ play_radio.py
@@ -2,6 +2,7 @@ from os import listdir
 from time import sleep
 from random import choice
 import pygame
+import espeak
 
 DIRECTORY = './recordings/OTRadio/'
 FILES = listdir(DIRECTORY)
@@ -19,11 +20,33 @@ def play(files_list):
     except:
         print 'Could not play file: ', current_file
 
+def speak(text):
+    """
+    Speak the TEXT value using espeak module
+    """
 
-while True:
-    playcount = 20
-    play(FILES)
-    while pygame.mixer.music.get_busy() == True and playcount:
-        sleep(1)
-        playcount -= 1
+def load_datetime():
+    """
+    Get the current date and time from the system. 
+    Check for presence of a hardware real-time-clock and compare result
+    to system time. If there is a discrepancy, try to find an NTP server
+    to verify actual real-world time. Order of precedence is; NTP, RTC, system-clock.
+    """
+
+def filter_files(files, parameter):
+    """
+    Return a subset of the files defined by parameter
+    """
+    return files
+
+# end of function declarations
+
+
+if __name__ == '__main__': #begin operation of radio
+    while True:
+        playcount = 20
+        play(FILES)
+        while pygame.mixer.music.get_busy() == True and playcount:
+            sleep(1)
+            playcount -= 1
         
