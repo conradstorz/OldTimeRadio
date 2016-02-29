47a66e43966ba205471830a2a580f4603f68395c
Conrad Storz <conradstorz@gmail.com>
2016-02-28 19:46:53 -0500

    abandoned namedtuple after remembering tuples are immutable

--- play_radio.py
+++ play_radio.py
@@ -6,7 +6,8 @@ import pygame
 from espeak import espeak
 import re
 from dateutil.parser import parse
-from collections import namedtuple
+#from collections import namedtuple
+from datetime import datetime
 
 DIRECTORY = './recordings/OTRadio/'
 FILES = listdir(DIRECTORY)
@@ -36,8 +37,6 @@ def parse_dates_in_library(library_directory):
         datedict[date] = name        
     return datedict
 
-
-Record = namedtuple('Recording', 'ID Filename URL Release_date Description Genre Length Num_of_plays Last_played Available Unavailable Was_interrupted')
 """
 These are the details to be tracked for each recording available to be played.
 Notes:
@@ -51,50 +50,54 @@ Notes:
     Num_of_plays(int) Starts at zero and increment for each time played
     Last_played(datetime) timestamp
     Available(bool) was this recording available the last time it was tried?
-    Unavailable(list(datetime)) a timestamp for each unplayable attempt
+    Unavailable_list(list(datetime)) a timestamp for each unplayable attempt
     Was_interrupted(list(tuple(datetime, length(int)))) A list of events where the user skipped a recording and the number of seconds played before interruption.
 """
 
 Identifier = 100000
-Recording_list = []
+Recording_dict = {}  # TODO create storage and retrieval routines
+
+# parse dates from filenames and build dictionary
 for f in FILES:
     date = parse_date(f)
     Identifier += 1
-    r = Record(ID=Identifier, Filename=f, URL='', Release_date=date, Description='', Genre='', Length=0, Num_of_plays=0,
-                Last_played=None, Available=False, Unavailable=[], Was_interrupted=[])
-    Recording_list.append(r)
+    r = {}
+    # create the minimum needed entries
+    r['ID'] = str(Identifier)
+    r['Filename'] = f
+    r['URL'] = DIRECTORY + f
+    r['Release_date'] = date
+    r['Description'] = None
+    r['Genre'] = None
+    r['Length'] = 0
+    r['Num_of_plays'] = 0
+    r['Last_played'] = None
+    r['Available'] = None
+    r['Unavailable_list'] = []
+    r['Was_interrupted'] = []
+    Recording_dict[r['URL']] = r
 
-print(Recording_list)
 
+# initialize pygame audio mixer
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.set_volume(1)
 
-def play(files_list, play_a_commercial=False):
+def play(file_url):
 
-    def a_commercial(file_name):
-        if 'Commercial' in file_name:
-            print(file_name, '... Has commercial in name.')
-            return True
-        return False
-
-    selected_file = choice(files_list)
-
-    # play the correct type of file
-    if play_a_commercial:
-        while not a_commercial(selected_file):
-            selected_file = choice(files_list)
-    else:
-        while a_commercial(selected_file):
-            selected_file = choice(files_list)
+    # we are ready to play the file
+    print('Playing: ', Recording_dict[file_url]['Filename'])
+    print('Parsed date from filename is: ', Recording_dict[file_url]['Release_date'])
+    Recording_dict[file_url]['Last_played'] = datetime.now()
 
     try:
-        pygame.mixer.music.load(DIRECTORY + selected_file)
-        print('Playing: ', selected_file)
+        pygame.mixer.music.load(file_url)
         pygame.mixer.music.play()
     except:
-        print('Could not play file: ', selected_file)
-    return selected_file
+        print('Could not play file: ', Recording_dict[file_url]['Filename'])
+        # record details in the dictionary
+        Recording_dict[file_url]['Unavailable_list'].append(datetime.now())
+    return Recording_dict[file_url]['Filename']
 
 def speak(text, gender='f3', emphasis='5', speed='150'):
     """
@@ -117,6 +120,25 @@ def filter_files(files, parameter):
     """
     return files
 
+def pick_a_random_file(files_list, play_a_commercial=False):
+
+    def a_commercial(file_name):
+        if 'Commercial' in file_name:
+            print(file_name, '... Has commercial in name.')
+            return True
+        return False
+
+    selected_file = choice(files_list)
+
+    # play the correct type of file
+    if play_a_commercial:
+        while not a_commercial(selected_file):
+            selected_file = choice(files_list)
+    else:
+        while a_commercial(selected_file):
+            selected_file = choice(files_list)
+
+    return DIRECTORY + selected_file
 
 # end of function declarations
 
@@ -124,12 +146,14 @@ def filter_files(files, parameter):
 if __name__ == '__main__': #begin operation of radio
     sleep(1)
     speak('welcome to the old time radio project')
+    print('System time is: ', datetime.now())
     sleep(5)
     we_should_play_a_commercial = False
     while True:
         playcount = 300
-        current_file = play(FILES, we_should_play_a_commercial)
-        print(current_file)
+        current_file_url = pick_a_random_file(FILES, we_should_play_a_commercial)
+        play(current_file_url)
+
         while pygame.mixer.music.get_busy() == True and playcount:
             sleep(1)
             if not playcount % 50:
